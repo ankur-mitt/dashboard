43dec6af30ad2dbba85ef090118749e945bef759
Ankur Mittal <mittankur1999@gmail.com>
2020-12-25 22:06:46 +0530

    dependency update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+    return (
+        <React.Fragment>
+            <button type="button" className="btn btn-primary">Primary</button>
+        </React.Fragment>
+    );
+}
+
+export default App;
